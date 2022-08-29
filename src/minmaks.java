a3f7aad3bce09b54b16270b64289311f9fa22237
eline00 <90403068+eline00@users.noreply.github.com>
2022-08-29 15:06:08 +0200

    Initial commit

---
+++ src/minmaks.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+
+public class minmaks {
+    public static int maks(int[] a) {
+        int maksverdi = a[0];
+        for (int i = 1; i < a.length; i++) {
+            int verdi = a[i];
+            if(verdi > maksverdi) {
+                maksverdi = verdi;
+            }
+        }
+        return maksverdi;
+    }
+
+    public static int min(int[] a) {
+        int minverdi = a[0];
+        for (int i = 1; i < a.length; i++) {
+            int verdi = a[i];
+            if (verdi < minverdi) {
+                minverdi = verdi;
+            }
+        }
+        return minverdi;
+    }
+
+    public static int[] minmaks(int[] a) {
+        int imaks = 0;
+        int imin = 0;
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] > a[imaks]) {
+                imaks = i;
+            }
+            else if (a[i] < a[imin]) {
+                imin = i;
+            }
+        }
+        int[] b = {imin, imaks};
+        return b;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] b = {1, 3, 2, 7, 5, 9, 6, 8, 10, 4};
+
+        System.out.println("Største verdi i er " + maks(a));
+        System.out.println("Minste verdi i er " + min(a));
+
+        System.out.println("Posisjonen til det minste og det største tallet er " + Arrays.toString(minmaks(a)));
+        System.out.println("Posisjonen til det minste og det største tallet er " + Arrays.toString(minmaks(b)));
+
+    }
+}
