843c78c054e70f613e7f019fae813a0dfedb7f8e
eline00 <90403068+eline00@users.noreply.github.com>
2022-09-05 12:49:52 +0200

    .

--- src/Uke35/Tabell.java
+++ src/Uke35/Tabell.java
@@ -46,10 +46,15 @@ public class Tabell     // Samleklasse for tabellmetoder
     // Metoden maks(int[] a, int fra, int til)   Programkode 1.2.1 b)
     public static int maks(int[] a, int fra, int til)
     {
-        if (fra < 0 || til > a.length || fra >= til)
-        {
-            throw new IllegalArgumentException("Illegalt intervall!");
-        }
+        if (a == null)
+            throw new NullPointerException
+                    ("Parametertabellen a er null!");
+
+        fratilKontroll(a.length, fra, til);
+
+        if (fra == til)
+            throw new NoSuchElementException
+                    ("fra(" + fra + ") = til(" + til + ") - tomt tabellintervall!");
 
         int m = fra;              // indeks til største verdi i a[fra:til>
         int maksverdi = a[fra];   // største verdi i a[fra:til>
@@ -75,10 +80,7 @@ public class Tabell     // Samleklasse for tabellmetoder
     // min-metodene - se Oppgave 1 i Avsnitt 1.2.1
     public static int min(int[] a, int fra, int til)
     {
-        if (fra < 0 || til > a.length || fra >= til)
-        {
-            throw new IllegalArgumentException("Illegalt intervall!");
-        }
+        fratilKontroll(a.length, fra, til);
 
         int m = fra;              // indeks til største verdi i a[fra:til>
         int minverdi = a[fra];   // største verdi i a[fra:til>
@@ -101,6 +103,9 @@ public class Tabell     // Samleklasse for tabellmetoder
     }
 
     public static void skriv(int[] a, int fra, int til) {
+
+        fratilKontroll(a.length, fra, til);
+
         for (int i = fra; i < til; i++) {
             System.out.println(a[i] + " ");
         }
@@ -111,6 +116,9 @@ public class Tabell     // Samleklasse for tabellmetoder
     }
 
     public static void skrivln(int[] a, int fra, int til) {
+
+        fratilKontroll(a.length, fra, til);
+
         skriv(a, 0, a.length);
         System.out.println();
     }
@@ -118,4 +126,66 @@ public class Tabell     // Samleklasse for tabellmetoder
     public static void skrivln(int[] a) {
         skrivln(a, 0, a.length);
     }
+
+    public static void fratilKontroll(int tablengde, int fra, int til)
+    {
+        if (fra < 0) // fra er negativ
+            throw new ArrayIndexOutOfBoundsException
+                    ("fra(" + fra + ") er negativ!");
+        if (til > tablengde) // til er utenfor tabellen
+            throw new ArrayIndexOutOfBoundsException
+                    ("til(" + til + ") > tablengde(" + tablengde + ")");
+        if (fra > til) // fra er større enn til
+            throw new IllegalArgumentException
+                    ("fra(" + fra + ") > til(" + til + ") - illegalt intervall!");
+    }
+
+    public static void vhKontroll(int tablengde, int v, int h)
+    {
+        if (v < 0)
+            throw new ArrayIndexOutOfBoundsException("v(" + v + ") < 0");
+        if (h >= tablengde)
+            throw new ArrayIndexOutOfBoundsException
+                    ("h(" + h + ") >= tablengde(" + tablengde + ")");
+        if (v > h + 1)
+            throw new IllegalArgumentException
+                    ("v = " + v + ", h = " + h);
+    }
+
+    public static int[] nestMaks(int[] a)  // legges i class Tabell
+    {
+        int n = a.length;   // tabellens lengde
+
+        if (n < 2) throw   // må ha minst to verdier!
+                new java.util.NoSuchElementException("a.length(" + n + ") < 2!");
+
+        int m = maks(a);  // m er posisjonen til tabellens største verdi
+
+        bytt(a, 0, m);  // bytter slik at maks ligger fremst (bytt(a, a.length-1, m) for å legge størst bakerst)
+
+        int nm = maks(a, 1, a.length);
+
+        if (nm == m) {
+            nm = 0;  //nest maks lå oppreinnelig først (nm = a.length-1 bakerst)
+        }
+
+        bytt(a, 0, m);  //bytter tilbake
+
+        return new int[] {m,nm};      // m i posisjon 0 , nm i posisjon 1
+
+    } // nestMaks
+
+    public static void sortering(int[] a) {
+        if (a.length < 2) throw   // må ha minst to verdier!
+                new java.util.NoSuchElementException("a.length(" + a.length + ") < 2!");
+
+        for (int i = a.length; i > 1; i--) {
+            int m = maks(a, 0, i);
+            bytt(a, i-1, m);
+        }
+    }
+
+
+
+
 }
