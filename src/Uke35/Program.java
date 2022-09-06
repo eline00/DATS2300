0e02c2dace9f582fc3768908e7da5fd3d814947a
eline00 <90403068+eline00@users.noreply.github.com>
2022-09-06 18:04:09 +0200

    .

--- src/Uke35/Program.java
+++ src/Uke35/Program.java
@@ -5,14 +5,14 @@ public class Program
     public static void main(String ... args)      // hovedprogram
     {
 
-        int[] a = Tabell.randPerm(20); // tilfeldig permutasjon av 1 . . 20
+        int[] a = Tabell1.randPerm(20); // tilfeldig permutasjon av 1 . . 20
         int[] b = new int[30];
 
-        Tabell.kopier(a, 0, b, (b.length - a.length)/2, a.length);
+        Tabell1.kopier(a, 0, b, (b.length - a.length)/2, a.length);
 
-        Tabell.skriv(a);
+        Tabell1.skriv(a);
         System.out.println();
-        Tabell.skriv(b);
+        Tabell1.skriv(b);
 
     } // main
 
