b68388fe73d95e7131b2d8c3379729dc1a4781c7
eline00 <90403068+eline00@users.noreply.github.com>
2022-09-06 17:35:00 +0200

    La til snu metoder og nestepermutasjon()

--- src/Uke35/Program.java
+++ src/Uke35/Program.java
@@ -6,13 +6,13 @@ public class Program
     {
 
         int[] a = Tabell.randPerm(20); // tilfeldig permutasjon av 1 . . 20
-        int[] b = Tabell.nestMaks(a);  // metoden returnerer en tabell
+        int[] b = new int[30];
 
-        int m = b[0], nm = b[1];       // m for maks, nm for nestmaks
+        Tabell.kopier(a, 0, b, (b.length - a.length)/2, a.length);
 
-        Tabell.skrivln(a);        // se Oppgave 5 i Avsnitt 1.2.2
-        System.out.print("Størst(" + a[m] + ") har posisjon " + m);
-        System.out.println(", nest størst(" + a[nm] + ") har posisjon " + nm);
+        Tabell.skriv(a);
+        System.out.println();
+        Tabell.skriv(b);
 
     } // main
 
--- src/Uke35/Tabell.java
+++ src/Uke35/Tabell.java
@@ -46,10 +46,15 @@ public class Tabell     // Samleklasse for tabellmetoder
     // Metoden maks(int[] a, int fra, int til)   Programkode 1.2.1 b)
     public static int maks(int[] a, int fra, int til)
     {
-        if (fra < 0 || til > a.length || fra >= til)
-        {
-            throw new IllegalArgumentException("Illegalt intervall!");
-        }
+        if (a == null)
+            throw new NullPointerException
+                    ("Parametertabellen a er null!");
+
+        fratilKontroll(a.length, fra, til);
+
+        if (fra == til)
+            throw new NoSuchElementException
+                    ("fra(" + fra + ") = til(" + til + ") - tomt tabellintervall!");
 
         int m = fra;              // indeks til største verdi i a[fra:til>
         int maksverdi = a[fra];   // største verdi i a[fra:til>
@@ -75,10 +80,7 @@ public class Tabell     // Samleklasse for tabellmetoder
     // min-metodene - se Oppgave 1 i Avsnitt 1.2.1
     public static int min(int[] a, int fra, int til)
     {
-        if (fra < 0 || til > a.length || fra >= til)
-        {
-            throw new IllegalArgumentException("Illegalt intervall!");
-        }
+        fratilKontroll(a.length, fra, til);
 
         int m = fra;              // indeks til største verdi i a[fra:til>
         int minverdi = a[fra];   // største verdi i a[fra:til>
@@ -101,8 +103,11 @@ public class Tabell     // Samleklasse for tabellmetoder
     }
 
     public static void skriv(int[] a, int fra, int til) {
+
+        fratilKontroll(a.length, fra, til);
+
         for (int i = fra; i < til; i++) {
-            System.out.println(a[i] + " ");
+            System.out.print(a[i] + " ");
         }
     }
 
@@ -111,6 +116,9 @@ public class Tabell     // Samleklasse for tabellmetoder
     }
 
     public static void skrivln(int[] a, int fra, int til) {
+
+        fratilKontroll(a.length, fra, til);
+
         skriv(a, 0, a.length);
         System.out.println();
     }
@@ -118,4 +126,133 @@ public class Tabell     // Samleklasse for tabellmetoder
     public static void skrivln(int[] a) {
         skrivln(a, 0, a.length);
     }
+
+    public static void fratilKontroll(int tablengde, int fra, int til)
+    {
+        if (fra < 0) // fra er negativ
+            throw new ArrayIndexOutOfBoundsException
+                    ("fra(" + fra + ") er negativ!");
+        if (til > tablengde) // til er utenfor tabellen
+            throw new ArrayIndexOutOfBoundsException
+                    ("til(" + til + ") > tablengde(" + tablengde + ")");
+        if (fra > til) // fra er større enn til
+            throw new IllegalArgumentException
+                    ("fra(" + fra + ") > til(" + til + ") - illegalt intervall!");
+    }
+
+    public static void vhKontroll(int tablengde, int v, int h)
+    {
+        if (v < 0)
+            throw new ArrayIndexOutOfBoundsException("v(" + v + ") < 0");
+        if (h >= tablengde)
+            throw new ArrayIndexOutOfBoundsException
+                    ("h(" + h + ") >= tablengde(" + tablengde + ")");
+        if (v > h + 1)
+            throw new IllegalArgumentException
+                    ("v = " + v + ", h = " + h);
+    }
+
+    public static int[] nestMaks(int[] a)  // legges i class Tabell
+    {
+        int n = a.length;   // tabellens lengde
+
+        if (n < 2) throw   // må ha minst to verdier!
+                new java.util.NoSuchElementException("a.length(" + n + ") < 2!");
+
+        int m = maks(a);  // m er posisjonen til tabellens største verdi
+
+        bytt(a, 0, m);  // bytter slik at maks ligger fremst (bytt(a, a.length-1, m) for å legge størst bakerst)
+
+        int nm = maks(a, 1, a.length);
+
+        if (nm == m) {
+            nm = 0;  //nest maks lå oppreinnelig først (nm = a.length-1 bakerst)
+        }
+
+        bytt(a, 0, m);  //bytter tilbake
+
+        return new int[] {m,nm};      // m i posisjon 0 , nm i posisjon 1
+
+    } // nestMaks
+
+    public static void sortering(int[] a) {
+        if (a.length < 2) throw   // må ha minst to verdier!
+                new java.util.NoSuchElementException("a.length(" + a.length + ") < 2!");
+
+        for (int i = a.length; i > 1; i--) {
+            int m = maks(a, 0, i);
+            bytt(a, i-1, m);
+        }
+    }
+
+    public static int[] nestMaks2(int[] a)   // en turnering
+    {
+        int n = a.length;                // for å forenkle notasjonen
+
+        if (n < 2) // må ha minst to verdier!
+            throw new IllegalArgumentException("a.length(" + n + ") < 2!");
+
+        int[] b = new int[2*n];          // turneringstreet
+        System.arraycopy(a,0,b,n,n);     // legger a bakerst i b
+
+        for (int k = 2*n-2; k > 1; k -= 2)   // lager turneringstreet
+            b[k/2] = Math.max(b[k],b[k+1]);
+
+        int maksverdi = b[1], nestmaksverdi = Integer.MIN_VALUE;
+
+        for (int m = 2*n - 1, k = 2; k < m; k *= 2)
+        {
+            int tempverdi = b[k+1];  // ok hvis maksverdi er b[k]
+            if (maksverdi != b[k]) { tempverdi = b[k]; k++; }
+            if (tempverdi > nestmaksverdi) nestmaksverdi = tempverdi;
+        }
+
+        return new int[] {maksverdi,nestmaksverdi}; // størst og nest størst
+
+    } // nestMaks
+
+    public static void kopier(int[] a, int i, int[] b, int j, int ant) {
+        for (int n = i + ant; i < n; ) {
+            b[j++] = a[i++];
+        }
+    }
+
+    public static void snu(int[] a, int v, int h)  // snur intervallet a[v:h]
+    {
+        while (v < h) {
+            bytt(a, v++, h--);
+        }
+    }
+
+    public static void snu(int[] a, int v)  // snur fra og med v og ut tabellen
+    {
+        snu(a, v, a.length - 1);
+    }
+
+    public static void snu(int[] a)  // snur hele tabellen
+    {
+        snu(a, 0, a.length - 1);
+    }
+
+    public static boolean nestePermutasjon(int[] a)
+    {
+        int i = a.length - 2;                    // i starter nest bakerst
+        while (i >= 0 && a[i] > a[i + 1]) {
+            i--;   // går mot venstre
+        }
+        if (i < 0) {
+            return false;                 // a = {n, n-1, . . . , 2, 1}
+        }
+
+        int j = a.length - 1;                    // j starter bakerst
+        while (a[j] < a[i]) {
+            j--;                 // stopper når a[j] > a[i]
+            bytt(a,i,j);
+            snu(a,i + 1);               // bytter og snur
+        }
+
+        return true;                             // en ny permutasjon
+    }
+
+
 }
